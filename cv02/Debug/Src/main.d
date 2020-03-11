afd74e1212541528c8cdba9c9d615b4e7c3f084a
Cristian Benites <xbenit01@vutbr.cz>
2020-03-11 20:54:00 +0100

    LMIA

--- cv02/Debug/Src/main.d
+++ cv02/Debug/Src/main.d
@@ -1,19 +1,19 @@
 Src/main.o: ../Src/main.c \
- D:/Documents/xobsit00/MMIA/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h \
- D:/Documents/xobsit00/MMIA/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h \
- D:/Documents/xobsit00/MMIA/cv01/CMSIS/Include/core_cm0.h \
- D:/Documents/xobsit00/MMIA/cv01/CMSIS/Include/core_cmInstr.h \
- D:/Documents/xobsit00/MMIA/cv01/CMSIS/Include/core_cmFunc.h \
- D:/Documents/xobsit00/MMIA/cv01/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
+ D:/www/xbenit01/MMIA/cv02/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ D:/www/xbenit01/MMIA/cv02/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ D:/www/xbenit01/MMIA/cv02/Include/core_cm0.h \
+ D:/www/xbenit01/MMIA/cv02/Include/core_cmInstr.h \
+ D:/www/xbenit01/MMIA/cv02/Include/core_cmFunc.h \
+ D:/www/xbenit01/MMIA/cv02/Device/ST/STM32F0xx/Include/system_stm32f0xx.h
 
-D:/Documents/xobsit00/MMIA/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+D:/www/xbenit01/MMIA/cv02/Device/ST/STM32F0xx/Include/stm32f0xx.h:
 
-D:/Documents/xobsit00/MMIA/cv01/CMSIS/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+D:/www/xbenit01/MMIA/cv02/Device/ST/STM32F0xx/Include/stm32f030x8.h:
 
-D:/Documents/xobsit00/MMIA/cv01/CMSIS/Include/core_cm0.h:
+D:/www/xbenit01/MMIA/cv02/Include/core_cm0.h:
 
-D:/Documents/xobsit00/MMIA/cv01/CMSIS/Include/core_cmInstr.h:
+D:/www/xbenit01/MMIA/cv02/Include/core_cmInstr.h:
 
-D:/Documents/xobsit00/MMIA/cv01/CMSIS/Include/core_cmFunc.h:
+D:/www/xbenit01/MMIA/cv02/Include/core_cmFunc.h:
 
-D:/Documents/xobsit00/MMIA/cv01/CMSIS/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+D:/www/xbenit01/MMIA/cv02/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
