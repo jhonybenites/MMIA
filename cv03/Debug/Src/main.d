f9660c319372854e8815e02114fce3d1ae87ec72
Cristian Benites <xbenit01@vutbr.cz>
2020-03-11 21:00:42 +0100

    LMIA

---
+++ cv03/Debug/Src/main.d
@@ -0,0 +1,22 @@
+Src/main.o: ../Src/main.c \
+ D:/www/xbenit01/MMIA/cv03/Device/ST/STM32F0xx/Include/stm32f0xx.h \
+ D:/www/xbenit01/MMIA/cv03/Device/ST/STM32F0xx/Include/stm32f030x8.h \
+ D:/www/xbenit01/MMIA/cv03/Include/core_cm0.h \
+ D:/www/xbenit01/MMIA/cv03/Include/core_cmInstr.h \
+ D:/www/xbenit01/MMIA/cv03/Include/core_cmFunc.h \
+ D:/www/xbenit01/MMIA/cv03/Device/ST/STM32F0xx/Include/system_stm32f0xx.h \
+ ../Src/sct.h
+
+D:/www/xbenit01/MMIA/cv03/Device/ST/STM32F0xx/Include/stm32f0xx.h:
+
+D:/www/xbenit01/MMIA/cv03/Device/ST/STM32F0xx/Include/stm32f030x8.h:
+
+D:/www/xbenit01/MMIA/cv03/Include/core_cm0.h:
+
+D:/www/xbenit01/MMIA/cv03/Include/core_cmInstr.h:
+
+D:/www/xbenit01/MMIA/cv03/Include/core_cmFunc.h:
+
+D:/www/xbenit01/MMIA/cv03/Device/ST/STM32F0xx/Include/system_stm32f0xx.h:
+
+../Src/sct.h:
---
+++ cv03b/Core/Inc/sct.h
@@ -0,0 +1,17 @@
+/*
+ * sct.h
+ *
+ *  Created on: Feb 19, 2020
+ *      Author: student
+ */
+
+#ifndef SCT_H_
+#define SCT_H
+
+void sct_led(uint32_t value);
+
+void sct_init(void);
+
+void sct_value(uint16_t);
+
+#endif /* SCT_H_ */
